710de7e9a84b29336ffc9868af1e751033f00350
付少松 <fushaosong@beyondcent.com>
2021-02-20 15:19:00 +0800

    update configmap
    snappy

--- controllers/persistentvolume_controller.go
+++ controllers/persistentvolume_controller.go
@@ -4,14 +4,18 @@ import (
 	"carina/utils"
 	"carina/utils/log"
 	"context"
+	"encoding/json"
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/util/workqueue"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"strings"
 	"time"
 )
 
@@ -34,12 +38,73 @@ func (r *PersistentVolumeReconciler) Reconcile(req ctrl.Request) (ctrl.Result, e
 	err := r.Get(ctx, req.NamespacedName, pv)
 	if err != nil {
 		log.Errorf("get pv info failed %s", req.Name)
+		return ctrl.Result{}, nil
+	}
+
+	if pv.Spec.CSI.Driver != utils.CSIPluginName {
+		return ctrl.Result{}, nil
+	}
+
+	nl := new(corev1.NodeList)
+	err = r.List(ctx, nl)
+	if err != nil {
 		return ctrl.Result{}, err
 	}
 
-	if pv.Spec.CSI.Driver == utils.CSIPluginName {
-		log.Infof("find pv create success %s", pv.Name)
-		return ctrl.Result{}, nil
+	nodeDevice := []map[string]string{}
+	for _, node := range nl.Items {
+		tmp := map[string]string{}
+		for key, v := range node.Status.Capacity {
+			if strings.HasPrefix(string(key), utils.DeviceCapacityKeyPrefix) {
+				tmp["capacity."+string(key)] = v.String()
+			}
+		}
+		for key, v := range node.Status.Allocatable {
+			if strings.HasPrefix(string(key), utils.DeviceCapacityKeyPrefix) {
+				tmp["allocatable."+string(key)] = v.String()
+			}
+		}
+		if len(tmp) > 0 {
+			tmp["nodeName"] = node.Name
+			nodeDevice = append(nodeDevice, tmp)
+		}
+	}
+	byteJson, err := json.Marshal(nodeDevice)
+	if err != nil {
+		log.Errorf("carina-node-storage json marshal failed %s", err.Error())
+		return ctrl.Result{}, err
+	}
+
+	cm := &corev1.ConfigMap{}
+	err = r.Get(ctx, client.ObjectKey{Namespace: "kube-system", Name: "carina-node-storage"}, cm)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			c := corev1.ConfigMap{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "ConfigMap",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "carina-node-storage",
+					Namespace: "kube-system",
+				},
+				Data: map[string]string{"node": string(byteJson)},
+			}
+			err = r.Create(ctx, &c)
+			if err != nil {
+				log.Errorf("update config map carina-vg failed %s", err.Error())
+				return ctrl.Result{}, err
+			}
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	cm.Data = map[string]string{"node": string(byteJson)}
+	err = r.Update(ctx, cm)
+	if err != nil {
+		log.Errorf("update config map carina-vg failed %s", err.Error())
+		return ctrl.Result{}, err
 	}
 
 	return ctrl.Result{}, nil
@@ -49,8 +114,8 @@ func (r *PersistentVolumeReconciler) Reconcile(req ctrl.Request) (ctrl.Result, e
 func (r *PersistentVolumeReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	pred := predicate.Funcs{
 		CreateFunc:  func(event.CreateEvent) bool { return true },
-		DeleteFunc:  func(event.DeleteEvent) bool { return false },
-		UpdateFunc:  func(event.UpdateEvent) bool { return false },
+		DeleteFunc:  func(event.DeleteEvent) bool { return true },
+		UpdateFunc:  func(event.UpdateEvent) bool { return true },
 		GenericFunc: func(event.GenericEvent) bool { return false },
 	}
 	return ctrl.NewControllerManagedBy(mgr).
