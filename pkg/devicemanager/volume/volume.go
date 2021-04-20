327671ea93d605f796aa01ea6f4efe589f0f521f
付少松 <fushaosong@beyondcent.com>
2021-04-20 17:07:18 +0800

    放宽检查存储容量检查条件

--- pkg/devicemanager/volume/volume.go
+++ pkg/devicemanager/volume/volume.go
@@ -40,7 +40,7 @@ func (v *LocalVolumeImplement) CreateVolume(lvName, vgName string, size, ratio u
 		return errors.New("cannot find device group info")
 	}
 
-	if vgInfo.VGFree-size < utils.DefaultReservedSpace {
+	if vgInfo.VGFree-size < utils.DefaultReservedSpace-1 {
 		log.Warnf("%s don't have enough space, reserved 10 g", vgName)
 		return errors.New("don't have enough space")
 	}
@@ -125,7 +125,7 @@ func (v *LocalVolumeImplement) ResizeVolume(lvName, vgName string, size, ratio u
 		return errors.New("cannot find device group info")
 	}
 
-	if vgInfo.VGFree-size < utils.DefaultReservedSpace {
+	if vgInfo.VGFree-size < utils.DefaultReservedSpace-1 {
 		log.Warnf("%s don't have enough space, reserved 10 g", vgName)
 		return errors.New("don't have enough space")
 	}
