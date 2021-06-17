b068396c409f5f898a88b545c103da0cd0ec40f2
付少松 <fushaosong@beyondcent.com>
2021-06-17 11:27:27 +0800

    修复由于uint64越界引起对容量显示不正确问题

--- pkg/deviceplugin/server.go
+++ pkg/deviceplugin/server.go
@@ -267,12 +267,15 @@ func (dp *CarinaDevicePlugin) getDeviceCapacity() ([]*v1beta1.Device, error) {
 	}
 
 	sizeGb := capacity.VGSize>>30 + 1
-	freeGb := (capacity.VGFree - utils.DefaultReservedSpace) >> 30
+	freeGb := uint64(0)
+	if capacity.VGFree > utils.DefaultReservedSpace {
+		freeGb = (capacity.VGFree - utils.DefaultReservedSpace) >> 30
+	}
 
 	// Capacity 这个是设备总资源数
 	// Allocatable 这个是资源可使用数，调度器使用这个指标，它的值是总量-预留-已使用
 	// 我们将已经使用的磁盘容量标记为unhealthy状态，如此在Node信息中看到allocatable在不断减少
-	for i := uint64(0); i < sizeGb; i++ {
+	for i := uint64(1); i <= sizeGb; i++ {
 		health := v1beta1.Healthy
 		if i > freeGb {
 			health = v1beta1.Unhealthy
