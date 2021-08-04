436eb233c2804221a4d39b0a9438c3bbc7e040a6
付少松 <fushaosong@beyondcent.com>
2021-08-04 11:15:12 +0800

    update license copyright

--- cmd/carina-node/run/run.go
+++ cmd/carina-node/run/run.go
@@ -1,6 +1,23 @@
+/*
+   Copyright @ 2021 fushaosong <fushaosong@beyondlet.com>.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package run
 
 import (
+	"context"
+	"errors"
 	carinav1 "github.com/bocloud/carina/api/v1"
 	"github.com/bocloud/carina/controllers"
 	"github.com/bocloud/carina/pkg/csidriver/csi"
@@ -9,8 +26,6 @@ import (
 	"github.com/bocloud/carina/pkg/csidriver/runners"
 	deviceManager "github.com/bocloud/carina/pkg/devicemanager"
 	"github.com/bocloud/carina/pkg/deviceplugin"
-	"context"
-	"errors"
 	"google.golang.org/grpc"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
--- pkg/csidriver/driver/node.go
+++ pkg/csidriver/driver/node.go
@@ -1,3 +1,18 @@
+/*
+   Copyright @ 2021 fushaosong <fushaosong@beyondlet.com>.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package driver
 
 import (
@@ -27,10 +42,10 @@ import (
 const (
 	// DeviceDirectory is a directory where Node service creates device files.
 	DeviceDirectory = "/dev/carina"
-	//mkfsCmd          = "/sbin/mkfs"
-	//mountCmd         = "/bin/mount"
-	//mountpointCmd    = "/bin/mountpoint"
-	//umountCmd        = "/bin/umount"
+	// mkfsCmd          = "/sbin/mkfs"
+	// mountCmd         = "/bin/mount"
+	// mountpointCmd    = "/bin/mountpoint"
+	// umountCmd        = "/bin/umount"
 	findmntCmd       = "/usr/bin/findmnt"
 	devicePermission = 0600 | unix.S_IFBLK
 )
