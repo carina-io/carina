9811f846e165eaa55af2dab9042a0800b65ed915
zhangkai8048 <wode804847802@163.com>
2022-03-27 16:21:00 +0800

    modify crd

--- api/api.go
+++ api/api.go
@@ -24,41 +24,41 @@ type PVInfo struct {
 }
 
 // Disk defines disk details
-type Disk struct {
-	Name string `json:"name"`
-	// mount point
-	MountPoint string `json:"mountPoint"`
-	// Size is the device capacity in byte
-	Size uint64 `json:"size"`
-	// status
-	State string `json:"state"`
-	// Type is disk type
-	Type string `json:"type"`
-	// 1 for hdd, 0 for ssd and nvme
-	Rotational string `json:"rotational"`
-	// ReadOnly is the boolean whether the device is readonly
-	Readonly bool `json:"readOnly"`
-	// Filesystem is the filesystem currently on the device
-	Filesystem string `json:"filesystem"`
-	// has used
-	Used uint64 `json:"used"`
+// type Disk struct {
+// 	Name string `json:"name"`
+// 	// mount point
+// 	MountPoint string `json:"mountPoint"`
+// 	// Size is the device capacity in byte
+// 	Size uint64 `json:"size"`
+// 	// status
+// 	State string `json:"state"`
+// 	// Type is disk type
+// 	Type string `json:"type"`
+// 	// 1 for hdd, 0 for ssd and nvme
+// 	Rotational string `json:"rotational"`
+// 	// ReadOnly is the boolean whether the device is readonly
+// 	Readonly bool `json:"readOnly"`
+// 	// Filesystem is the filesystem currently on the device
+// 	Filesystem string `json:"filesystem"`
+// 	// has used
+// 	Used uint64 `json:"used"`
 
-	Capacity  string      `json:"capacity,omitempty"`
-	Available string      `json:"available,omitempty"`
-	Partition []Partition `json:"partition,omitempty"`
-	FreeSpace []Partition `json:"freespace,omitempty"`
-}
+// 	Capacity  string      `json:"capacity,omitempty"`
+// 	Available string      `json:"available,omitempty"`
+// 	Partition []Partition `json:"partition,omitempty"`
+// 	FreeSpace []Partition `json:"freespace,omitempty"`
+// }
 
 // Partition defines disk partition details
-type Partition struct {
-	Number     string `json:"number,omitempty"`
-	Start      string `json:"start,omitempty"`
-	End        string `json:"end,omitempty"`
-	Size       string `json:"size,omitempty"`
-	Filesystem string `json:"filesystem,omitempty"`
-	Name       string `json:"name,omitempty"`
-	Flags      string `json:"flags,omitempty"`
-}
+// type Partition struct {
+// 	Number     string `json:"number,omitempty"`
+// 	Start      string `json:"start,omitempty"`
+// 	End        string `json:"end,omitempty"`
+// 	Size       string `json:"size,omitempty"`
+// 	Filesystem string `json:"filesystem,omitempty"`
+// 	Name       string `json:"name,omitempty"`
+// 	Flags      string `json:"flags,omitempty"`
+// }
 
 // Raid defines raid details
 type Raid struct {
